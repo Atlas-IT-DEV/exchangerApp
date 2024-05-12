89c31dc03d21460a281202a5ce2821e5978b70f5
ElEgEv <112943269+ElEgEv@users.noreply.github.com>
2024-05-12 23:10:36 +0400

    Add logger.

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -3,21 +3,28 @@ import os
 import requests
 from dotenv import load_dotenv
 
+import app_logs
+
 load_dotenv()
 
 bot = TeleBot(os.getenv('ADMINS_KEY'))
+
+logger = app_logs.adminLogger
 
 # fast api url
 BASE_URL = 'http://127.0.0.1:8008'
 
 @bot.message_handler(commands=['start'])
-def start(m, res=False):
-    username = m.from_user.username
-    bot.send_message(m.chat.id, f'Добро пожаловать в бот для администраторов, {username}!')
+def start(message, res=False):
+    username = message.from_user.username
+    logger.log('info', (f"User {message.from_user.id} launched the bot with the command /start"))
+    bot.send_message(message.chat.id, f'Добро пожаловать в бот для администраторов, {username}!')
     
 @bot.message_handler(commands=['text'])
 def answer(message):
-    bot.send_message(message.chat.id, 'example')
+    text = message.text.split(' ', 1)[1]  # Получаем текст после команды '/' и пробела 
+    logger.log('info', (f"Get message from user {message.from_user.id}: {text}"))
+    bot.send_message(message.chat.id, text)
     
 @bot.message_handler(commands=['hello'])
 def getHello(message):
