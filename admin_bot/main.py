59b2d15d06c694ccaa518902aa0778c92de7ba7b
ElEgEv <112943269+ElEgEv@users.noreply.github.com>
2024-05-12 21:59:23 +0400

    Connect to api by url

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -1,19 +1,29 @@
 from telebot import TeleBot
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 bot = TeleBot(os.getenv('ADMINS_KEY'))
 
+# fast api url
+BASE_URL = 'http://127.0.0.1:8008'
+
 @bot.message_handler(commands=['start'])
 def start(m, res=False):
     username = m.from_user.username
-    bot.send_message(m.chat.id, f'Добро пожаловать в бот для администраторов {username}')
+    bot.send_message(m.chat.id, f'Добро пожаловать в бот для администраторов, {username}!')
     
 @bot.message_handler(commands=['text'])
 def answer(message):
     bot.send_message(message.chat.id, 'example')
+    
+@bot.message_handler(commands=['hello'])
+def getHello(message):
+    response = requests.get(f"{BASE_URL}/hello")
+    data = response.json()
+    bot.send_message(message.chat.id, data['message'])
 
 if __name__ == '__main__':
     bot.infinity_polling()
